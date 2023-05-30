848dee43eeeb6b73ca07fa54f3441c75a69b3b67
Tega <beniyoke@gmail.com>
2023-05-30 09:47:20 +0100

    v.1.0.0

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BaseConverter',
+    version='1.0.0',
+    description='<description>',
+    author='Akpojiyovwi Tega',
+    author_email='beniyoke@gmail.com',
+    url='https://github.com/t-ega/BaseConverter',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
